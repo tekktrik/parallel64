ecbbd2b4f4fa80c9aacddbbe9ea0a9f40fdc938a
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-08-25 10:53:47 -0400

    Dev-25AUG2021 P2
    Removing custom errors, adding writing/reading to ECR

--- parallel64/extended_port.py
+++ parallel64/extended_port.py
@@ -23,28 +23,28 @@ class ExtendedPort(SimplePort):
         def int_to_binstr(cls, int_data):
             return '{:08b}'.format(int_data)
             
-    class CommunicationMode(Enum):
-        
-        SPP = 0
-        EPP = 1
+    #class CommunicationMode(Enum):
+    #    
+    #    SPP = 0
+    #    EPP = 1
             
     def __init__(self, spp_base_address, ecp_base_address, windll_location, start_mode=None):
         super.__init__(spp_base_address, windll_location)
         self._ecr_address = int(ecp_base_address, 16) + 2
         
-    def setCommunicationMode(self, comm_mode):
-        if comm_mode is self.CommunicationMode.SPP:
-            self.setSPPCommunicationMode()
-        elif comm_mode os self.CommunicationMode.EPP
-            self.setEPPCommunicationMode()
-        else:
-            raise port_errors.InvalidCommunicationMode("Received invalid communication mode, mode not changed", comm_mode)
+    #def setCommunicationMode(self, comm_mode):
+    #    if comm_mode is self.CommunicationMode.SPP:
+    #        self.setSPPCommunicationMode()
+    #    elif comm_mode os self.CommunicationMode.EPP
+    #        self.setEPPCommunicationMode()
+    #    else:
+    #        raise port_errors.InvalidCommunicationMode("Received invalid communication mode, mode not changed", comm_mode)
         
     def setSPPCommunicationMode(self):
-        self._parallel_port.DlPortWritePortUchar(self._ecr_address, 0)
+        self.writeECRData(0)
         
     def setEPPCommunicationMode(self):
-        self._parallel_port.DlPortWritePortUchar(self._ecr_address, (1 << 7))
+        self.writeECRData(1 << 7)
         
     @classmethod
     def fromJSON(cls, json_filepath):
@@ -67,3 +67,9 @@ class ExtendedPort(SimplePort):
     def readEPPData(self):
         self._parallel_port.DlPortWritePortUchar(self._control_address, 0b00100100)
         return self._parallel_port.DlPortReadPortUchar(self._epp_data_address)
+        
+    def writeECRData(self, data):
+        self._parallel_port.DlPortWritePortUchar(self._ecr_address, (1 << 7))
+        
+    def readECRData(self):
+        return self._parallel_port.DlPortReadPortUchar(self._ecr_address)
