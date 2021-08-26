40159dea5ee7a75874e0c177bef2ad30ec53c636
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-08-26 16:24:02 -0400

    Building out GPIOPort

--- parallel64/gpio_port.py
+++ parallel64/gpio_port.py
@@ -1,23 +1,64 @@
 import ctypes
 import json
+from enum import Enum
 from simple_port import SimplePort
 
 class GPIOPort(SimplePort):
 
-    def readDataBitIndex(self, bit_index):
-        if (bit_index >= 0) and (bit_index <=7) and isinstance(bit_index, int):
-            byte_read = self._parallel_port.readSPPData()
-            bit_mask = (1 << bit_index)
-            return (bit_mask & byte_read) >> bit_index
-        else:
-            if (bit_index < 0) or (bit_index > 7):
-                raise IndexError("Bit requested "+str(bit_index)+"is not accessible")
-            if not isinstance(bit_index, int):
-                raise TypeError("Bit requested must be an integer")
+    class Pins:
+        
+        def __init__(self, pin_number, bit_index, register, hw_inverted=False):
+            self.pin_number = pin_number
+            self.bit_index = bit_index
+            self.register = register
+            self.hw_inverted = hw_inverted
+
+    # class GPIOPins(Enum):
+        
+        # def isHardwareInverted(self):
+            # return self.hw_inverted
+        
+        # @classmethod
+        # def getViablePinNumbers(cls):
+            # return set(pin.pin_number for pin in cls)
+            
+        # @classmethod
+        # def isViablePin(cls, pin_number):
+            # return pin_number in cls.getViablePinNumbers()
+        
+    class OutputPins(Pins):
+        STROBE = self.Pin(1, 0, self._control_address, True)
+        AUTO_LINEFEED = self.Pin(14, 1, self._control_address, True)
+        INITIALIZE = self.Pin(16, 2, self._control_address, False)
+        SELECT_PRINTER = self.Pin(17, 3, self._control_address, True)
+        
+    class InputPins(Pins):
+        ACK = 10
+        BUSY = 11
+        PAPER_OUT = 12
+        SELECT_IN = 13
+        ERROR = 15
     
-    def readPin(self, pin_number):
+    class InputOutputPins(Pins):
+    
+        D0 = 2
+        D1 = 3
+        D2 = 4
+        D3 = 5
+        D4 = 6
+        D5 = 7
+        D6 = 8
+        D7 = 9
+    
+    def readPin(self, pin):
+        if isinstance(pin, int):
+            try:
+                
         try:
-            if (pin_number >= 2) and (pin_number <= 9):
-                return self.readDataBitIndex(pin_number - 1)
+            if ins:
+                bit_index = bit_index - 2
+                byte_read = self._parallel_port.readSPPData()
+                bit_mask = (1 << bit_index)
+                return (bit_mask & byte_read) >> bit_index
         except TypeError:
             raise TypeError("Pin requested must be an integer")
--- parallel64/standard_port.py
+++ parallel64/standard_port.py
@@ -3,24 +3,6 @@ import json
 
 class StandardPort:
 
-    class DataConverter:
-        
-        @classmethod
-        def hexstr_to_int(cls, trimmed_hex_str):
-            return int("0x" + trimmed_hex_str, 16)
-        
-        @classmethod
-        def binstr_to_int(cls, trimmed_bin_str):
-            return int("0b" + trimmed_bin_str, 2)
-            
-        @classmethod
-        def int_to_hexstr(cls, int_data):
-            return ''.join('%02x'%int(hex(int_data), 16))
-            
-        @classmethod
-        def int_to_binstr(cls, int_data):
-            return '{:08b}'.format(int_data)
-
     def __init__(self, spp_base_address, windll_location):
     
         self._spp_data_address = spp_base_address
@@ -73,22 +55,3 @@ class StandardPort:
         self.resetControlForSPPHandshake()
         self.setReverseDirection()
         return self._parallel_port.DlPortReadPortUchar(self._spp_data_address)
-
-    def readDataBitIndex(self, bit_index):
-        if (bit_index >= 0) and (bit_index <=7) and isinstance(bit_index, int):
-            byte_read = self._parallel_port.readSPPData()
-            bit_mask = (1 << bit_index)
-            return (bit_mask & byte_read) >> bit_index
-        else:
-            if (bit_index < 0) or (bit_index > 7):
-                raise IndexError("Bit requested "+str(bit_index)+"is not accessible")
-            if not isinstance(bit_index, int):
-                raise TypeError("Bit requested must be an integer")
-    
-    def readPin(self, pin_number):
-        try:
-            if (pin_number >= 2) and (pin_number <= 9):
-                return self.readDataBitIndex(pin_number - 1)
-        except TypeError:
-            raise TypeError("Pin requested must be an integer")
-                
