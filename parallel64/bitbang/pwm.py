be78875172ca6222b1f9250217f4bbb0c87fc8b9
Tekktrik <tekktrik@gmail.com>
2021-08-28 20:36:58 -0400

    Finished prototyping PWM class

--- parallel64/bitbang/pwm.py
+++ parallel64/bitbang/pwm.py
@@ -1,13 +1,14 @@
 import time
-import multiprocessing
+import multiprocessing as mp
 
 class PWM:
     
-    def __init__(self, gpio_port, pwm_pin, duty_cycle, cycle_time):
+    def __init__(self, gpio_port, pwm_pin, duty_cycle=0, cycle_time=0.001):
         self._port = gpio_port
         self._pin = pwm_pin
         self._duty_cycle = duty_cycle
         self.cycle_time
+        self._pwm_process = None
         
     @property
     def pin(self):
@@ -31,6 +32,27 @@ class PWM:
         else:
             raise ValueError("Duty cycle must be between 0 and 1")
             
-    def startCycle(self):
-        pass
+    def _pwmCycle(self):
+        on_time = cycle_time*self._duty_cycle
+        off_time = cycle_time - on_time
+        while True:
+            self._port.writePin(self._pin, True)
+            on_delay = time.monotonic() + on_time
+            while time.monotonic() < on_delay:
+                pass
+            self._port.writePin(self._pin, False)
+            off_delay = time.monotonic() + off_time
+            while time.monotonic() < off_delay
+                pass
             
+            
+    def startCycle(self):
+        self._pwm_process = mp.Process(target=_pwmCycle)
+        self._pwm_process.daemon = True
+        self._pwm_process.start()
+        
+    def endCycle(self):
+        self._pwm_process.terminate()
+        while self._pwm_process.is_alive:
+            pass
+        self._pwm_process.join()
