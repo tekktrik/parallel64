2c399a2f631c943db1e82161fa8b0822d0b1e917
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-09-13 18:11:52 -0400

    Removed non-PWM bitbang files

--- parallel64/bitbang/__init__.py
+++ parallel64/bitbang/__init__.py
@@ -1,7 +1,4 @@
-#from parallel64.bitbang.i2c import I2C
-#from parallel64.bitbang.pwm import PWM
 from parallel64.bitbang.bitbang_pwm import PWM
-from parallel64.bitbang.bitbang_i2c import I2C
-
+#from parallel64.bitbang.bitbang_i2c import I2C
 #from bitbang.spi import SPI
 #from bitbang.uart import UART
