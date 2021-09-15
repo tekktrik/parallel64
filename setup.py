41d41fbf3f53dbd5bcbfc13bb0d65d606e7216cb
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-09-15 13:53:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name = 'parallel64',
-    packages = ['parallel64', 'parallel64.bitbang', 'parallel64.inpoutdlls'],
-    version = 'v0.4.12',  # Ideally should be same as your GitHub release tag varsion
+    packages = ['parallel64'],
+    version = 'v0.4.13',  # Ideally should be same as your GitHub release tag varsion
     description = 'Python package for working with parallel ports in a 64-bit Windows environment',
     author = 'Alec Delaney',
     author_email = 'tekktrik@gmail.com',
     url = 'https://github.com/tekktrik/parallel64',
-    download_url = 'https://github.com/tekktrik/parallel64/archive/refs/tags/v0.4.12.tar.gz',
+    download_url = 'https://github.com/tekktrik/parallel64/archive/refs/tags/v0.4.13.tar.gz',
     include_package_data = True,
     package_data = {"bitbang": ['parallel64.bitbang.bitbang_pwm.pyd']},
     keywords = ['parallel', 'port', 'spp', 'epp', 'ecp'],
