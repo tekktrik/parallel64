fde38a53fd406dd99609155527ec1238eefc3f2d
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-09-15 14:17:16 -0400

    Update MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name = 'parallel64',
     packages = ['parallel64', 'parallel64.bitbang', 'parallel64.inpoutdlls'],
-    version = 'v0.4.17',  # Ideally should be same as your GitHub release tag varsion
+    version = 'v0.4.19',  # Ideally should be same as your GitHub release tag varsion
     description = 'Python package for working with parallel ports in a 64-bit Windows environment',
     author = 'Alec Delaney',
     author_email = 'tekktrik@gmail.com',
     url = 'https://github.com/tekktrik/parallel64',
-    download_url = 'https://github.com/tekktrik/parallel64/archive/refs/tags/v0.4.17.tar.gz',
+    download_url = 'https://github.com/tekktrik/parallel64/archive/refs/tags/v0.4.19.tar.gz',
     include_package_data = True,
     package_data = {"bitbang_pwm": ['parallel64.bitbang.bitbang_pwm.pyd']},
     keywords = ['parallel', 'port', 'spp', 'epp', 'ecp'],
