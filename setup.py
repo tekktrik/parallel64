b42a6be1aed0c0a7e044c9dce5438d70570a3ea7
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-09-15 11:36:45 -0400

    Updated setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'parallel64',
+    packages = ['parallel64'],
+    version = 'v0.4.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Python package for working with parallel ports in a 64-bit Windows environment',
+    author = 'Alec Delaney',
+    author_email = 'tekktrik@gmail.com',
+    url = 'https://github.com/tekktrik/parallel64',
+    download_url = 'https://github.com/tekktrik/parallel64/archive/refs/tags/v0.4.0.tar.gz',
+    keywords = ['parallel', 'port', 'spp', 'epp', 'ecp'],
+    classifiers = [],
+)
