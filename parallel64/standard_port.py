6ac4a226672bb5900f632c7b7cf47ab755902ffa
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-10-17 17:44:25 -0400

    Don't require windll_location in factory method JSON

--- parallel64/standard_port.py
+++ parallel64/standard_port.py
@@ -38,8 +38,11 @@ class StandardPort:
             json_contents = json.load(json_file)
         try:
             spp_base_add = int(json_contents["spp_base_address"], 16)
-            windll_loc = json_contents["windll_location"]
-            return cls(spp_base_add, windll_location)
+            try:
+                windll_loc = json_contents["windll_location"]
+            except KeyError:
+                windll_loc = None
+            return cls(spp_base_add, windll_loc)
         except KeyError as err:
             raise KeyError("Unable to find " + str(err) + " parameter in the JSON file, see reference documentation")
         
