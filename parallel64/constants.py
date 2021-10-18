30665d907708c3c4595093148888a03be550d6d3
Alec Delaney <tekktrik@gmail.com>
2021-10-18 10:50:42 -0400

    Add docstrings to constants

--- parallel64/constants.py
+++ parallel64/constants.py
@@ -2,12 +2,19 @@ from enum import Enum
 
 class Direction(Enum):
     '''Enum class representing the current direction of the port
+    
+    Used with :class:`parallel64.StandardPort`
     '''
 
     REVERSE = 0
     FORWARD = 1
 
 class CommMode(Enum):
+    '''Enum class representing the various protocols the ECR might be configureable for (port-dependent). 
+    Note that more available modes may be added as this project progresses.
+
+    Used with :class:`parallel64.ExtendedPort`
+    '''
     
     SPP = 0
     BYTE = 1
